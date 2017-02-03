21edff5fc5df0a9ff1d0c636db76c5138b2dd9c4
David Manouchehri <manouchehri@riseup.net>
2017-02-03 14:08:44 -0500

    Sanity check.

--- examples/catalyst/solve.py
+++ examples/catalyst/solve.py
@@ -10,9 +10,13 @@ proj = angr.Project('catalyst', load_options={"auto_load_libs": False})
 
 def nuller(state):
 	pass
+	# return 0
 
 import simuvex
 # proj.hook_symbol('puts', simuvex.SimProcedures['stubs']['ReturnUnconstrained'])
+# proj.hook_symbol('putchar', simuvex.SimProcedures['stubs']['ReturnUnconstrained'])
+# proj.hook_symbol('printf', simuvex.SimProcedures['stubs']['ReturnUnconstrained'])
+
 proj.hook_symbol('puts', nuller)
 proj.hook_symbol('putchar', nuller)
 proj.hook_symbol('printf', nuller)
@@ -26,3 +30,7 @@ path_group = proj.factory.path_group(state)
 
 FIND_ADDR=0x400896
 path_group.explore(find=FIND_ADDR)
+e = path_group.errored[0]
+
+from IPython import embed
+embed()
