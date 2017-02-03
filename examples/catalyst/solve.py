7c31f66559174f8899e016f5ec2e596e502c2939
David Manouchehri <manouchehri@riseup.net>
2017-02-03 13:16:49 -0500

    Work in progress, not working yet.

---
+++ examples/catalyst/solve.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+# Author: David Manouchehri
+# Binary origins unknown
+
+import angr
+
+proj = angr.Project('catalyst', load_options={"auto_load_libs": False}) 
+
+def nuller(state):
+	pass
+
+import simuvex
+# proj.hook_symbol('puts', simuvex.SimProcedures['stubs']['ReturnUnconstrained'])
+proj.hook_symbol('puts', nuller)
+proj.hook_symbol('putchar', nuller)
+proj.hook_symbol('printf', nuller)
+
+MAIN_ADDR=0x400d93
+state = proj.factory.blank_state(addr=MAIN_ADDR) # Beginning of function
+# state = proj.factory.full_init_state(addr=MAIN_ADDR) # Beginning of function
+
+# initial_path = proj.factory.path(state)  # Is this needed?
+path_group = proj.factory.path_group(state)
+
+FIND_ADDR=0x400896
+path_group.explore(find=FIND_ADDR)
