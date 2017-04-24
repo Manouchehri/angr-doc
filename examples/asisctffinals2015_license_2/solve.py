4417adaa740d21c201482fd65d70e660cdb274a4
David Manouchehri <manouchehri@riseup.net>
2017-04-23 22:34:55 -0400

    Update solve.py

--- examples/asisctffinals2015_license_2/solve.py
+++ examples/asisctffinals2015_license_2/solve.py
@@ -4,26 +4,40 @@
 # Runtime: ~? minutes
 
 import angr, string, simuvex
-# START_ADDR=
-# FIND_ADDR=
-# AVOID_ADDRS=()
+START_ADDR=0x4009c0
+FIND_ADDR=0x400e93
+AVOID_ADDRS=(0x400a85, 0x4010df, 0x400bb1, 0x400b6d, 0x4010f3, 0x400ebf, 0x40110c, 0x400b8f)
 
 import IPython # Remove this before merging.
-
 
 def main():
 	proj = angr.Project('./license', load_options={"auto_load_libs": False})
 
-	key_length = 1024
+	key_length = 8196
 	key_name = "_a\nb\tc_"
 
-	state = proj.factory.entry_state(addr=START_ADDR, args=["./definitely_maybe_fb594f5bd4bfaf0c685a69b5bae84573", key_name])
+	# state = proj.factory.full_init_state()
+	# state = proj.factory.blank_state(remove_options={simuvex.s_options.LAZY_SOLVES})
+	state = proj.factory.entry_state(addr=START_ADDR, remove_options={simuvex.s_options.LAZY_SOLVES}, args=["./license"])
 
 	key = state.se.BVS('key_bytes', key_length * 8)
 	content = simuvex.SimSymbolicMemory(memory_id='file_{}'.format(key_name))
+
+	#for byte in key.chop(8):
+	#	state.add_constraints(byte != '\x00') # null
+	#	state.add_constraints(byte >= ' ') # '\x20'
+	#	state.add_constraints(byte <= '~') # '\x7e'
+
+	# state.add_constraints(key.chop(8)[0] == 'A')
+	# state.add_constraints(key.chop(8)[1] == 'S')
+	# state.add_constraints(key.chop(8)[2] == 'I')
+	# state.add_constraints(key.chop(8)[3] == 'S')
+	# state.add_constraints(key.chop(8)[4] == '{')
+
 	content.set_state(state)
 	content.store(0, key)
-	key_file = simuvex.SimFile(key_name, 'rw', content=content, size=key_length)
+
+	key_file = simuvex.SimFile(key_name, 'r', content=content, size=key_length)
 	fs = {
 		key_name: key_file
 	}
@@ -45,13 +59,18 @@ def main():
 			return False
 
 	while path_group.active:
-		path_group.explore(find=corrector, avoid=avoider, n=10)
+		path_group.explore(find=FIND_ADDR, avoid=AVOID_ADDRS, n=1)
+		# path_group.explore(find=correcter, avoid=avoider, n=1)
 		try:
 			for a in path_group.active:
-				print filter(lambda x: x in string.printable, a.state.se.state.posix.dumps(3)).split()[0]
-				print a.state.se.state.posix.dumps(1)
+				print a
+				print str(a.callstack_backtrace)
+				# print a.state.se.state.posix.dumps(1)
+				print repr(a.state.se.state.posix.dumps(3))
+				#print filter(lambda x: x in string.printable, a.state.se.state.posix.dumps(3)).split()[0]
 		except:
 			pass
+
 		print path_group
 		# IPython.embed()
 
@@ -76,3 +95,4 @@ def test():
 
 if __name__ == '__main__':
 	print(main())
+
